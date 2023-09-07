0770a23d505dcc7a7f4f3035c5476fbc11d4b826
jvaugha3038 <jvaugha3038@charlottesvilleschools.org>
2023-09-07 13:56:26 -0400

    coded

---
+++ raspberry-pi/countdown2.py
@@ -0,0 +1,20 @@
+from time import sleep
+import board
+import digitalio #the stuff again
+
+red = digitalio.DigitalInOut(board.GP19)
+red.direction = digitalio.Direction.OUTPUT
+green = digitalio.DigitalInOut(board.GP17)
+green.direction = digitalio.Direction.OUTPUT #more LED setup
+
+green.value=False
+for i in range(10,0,-1): #The Loop
+    print(str(i))
+    red.value=True
+    sleep(.5)
+    red.value=False
+    sleep(.5)
+print("Liftoff")
+green.value=True
+sleep(3) #to keep the led on for 3 seconds
+
