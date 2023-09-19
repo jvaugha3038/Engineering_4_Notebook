79b314eb82ac3e5eb2733f9d58dbd76b9fe19506
jvaugha3038 <jvaugha3038@charlottesvilleschools.org>
2023-09-19 13:40:14 -0400

    commented

--- raspberry-pi/crash2.py
+++ raspberry-pi/crash2.py
@@ -12,13 +12,13 @@ red.direction = Direction.OUTPUT
 sda_pin = board.GP14
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
-mpu = adafruit_mpu6050.MPU6050(i2c)
+mpu = adafruit_mpu6050.MPU6050(i2c) #the setup soup
 
-while True:
+while True: #the Loop
     acc = mpu.acceleration
-    print("X = ["+str(acc[0])+"]| Y = ["+str(acc[1])+"]| Z = ["+str(acc[2])+"]")
+    print("X = ["+str(acc[0])+"]| Y = ["+str(acc[1])+"]| Z = ["+str(acc[2])+"]") #prints acceleration values using the archaic str() method
     if acc[2] <= 0:
-        red.value = True
+        red.value = True #if number small light go
     else:
-        red.value = False
-    sleep(0.1)
+        red.value = False #if number big light no
+    sleep(0.1) #wait
