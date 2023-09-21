b6ae13e4d13cc554f39c0da1906862859c4706b2
jvaugha3038 <jvaugha3038@charlottesvilleschools.org>
2023-09-21 13:33:16 -0400

    removed a line

--- raspberry-pi/countdown.py
+++ raspberry-pi/countdown.py
@@ -1,10 +1,10 @@
+#type:ignore
 from time import sleep
 import board
 import digitalio #all the imports
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT #led setup
-count=10
 
 for i in range(10,0,-1): #repeats 10 times
     print(str(i))
