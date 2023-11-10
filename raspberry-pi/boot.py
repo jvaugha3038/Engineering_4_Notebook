b94e2beea1a81898736cdab3e7c92dc74cfec90b
jvaugha3038 <jvaugha3038@charlottesvilleschools.org>
2023-11-10 14:01:14 -0500

    boot

--- raspberry-pi/boot.py
+++ raspberry-pi/boot.py
@@ -13,16 +13,16 @@ import digitalio
 import storage
 import time
 
-write_pin = digitalio.DigitalInOut(board.GP0)
-write_pin.direction = digitalio.Direction.INPUT
-write_pin.pull = digitalio.Pull.UP
+GP0 = digitalio.DigitalInOut(board.GP0)
+GP0.direction = digitalio.Direction.INPUT
+GP0.pull = digitalio.Pull.UP
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 time.sleep(2)
 
 # If write pin is connected to ground on start-up, CircuitPython can write to CIRCUITPY filesystem.
-if not write_pin.value: # Data Mode, shown by 10 short blinks
+if not GP0.value: # Data Mode, shown by 10 short blinks
     storage.remount("/", readonly=False)
     for i in range(10):
         led.value = True
